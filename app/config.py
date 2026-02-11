2c8e604029051a95ba5b77c28d1d542e6323913c
janailsonf-a <janailsonfalmeida@gmail.com>
2026-02-11 20:22:18 -0300

    Remove IDE files from repository

--- app/config.py
+++ app/config.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 @dataclass(frozen=True)
 class Settings:
-    root_dir: str = os.getenv("ROOT_DIR", "/home/janailson/arquivos_teste")
-    db_path = "/var/lib/indexador/file_index.db"
-    host: str = os.getenv("HOST", "127.0.0.1")
-    port: int = int(os.getenv("PORT", "8000"))
+    root_dir: str = os.getenv("ROOT_DIR")
+    db_path = "DB_PATH"
+    host: str = os.getenv("HOST", "0.0.0.0")
+    port: int = int(os.getenv("PORT", "9001"))
     max_results: int = int(os.getenv("MAX_RESULTS", "50"))
 
 settings = Settings()
