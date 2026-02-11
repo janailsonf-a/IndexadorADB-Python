41084abba78d628e99a6293f9d97b6eadcc0f085
janailsonf-a <janailsonfalmeida@gmail.com>
2026-02-11 20:51:08 -0300

    ajuste no .env.example

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ load_dotenv()
 @dataclass(frozen=True)
 class Settings:
     root_dir: str = os.getenv("ROOT_DIR")
-    db_path = "DB_PATH"
+    db_path: str = os.getenv("DB_PATH")
     host: str = os.getenv("HOST", "0.0.0.0")
     port: int = int(os.getenv("PORT", "9001"))
     max_results: int = int(os.getenv("MAX_RESULTS", "50"))
